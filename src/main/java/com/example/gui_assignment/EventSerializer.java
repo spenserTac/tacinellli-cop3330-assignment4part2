a265ced8aa795ab29e8b04d58af56fb5caecc2e6
spenserTac <spenserdt@gmail.com>
2021-11-15 17:35:15 -0500

    first real commit

---
+++ src/main/java/com/example/gui_assignment/EventSerializer.java
@@ -0,0 +1,66 @@
+
+
+package com.example.gui_assignment;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public final class EventSerializer {
+    private static final String DATA_PATH = "data.bin";
+
+    public static void serialize(Object[] events) throws IOException {
+        ObjectOutputStream serializer = new ObjectOutputStream(new FileOutputStream("data.bin"));
+
+        try {
+            serializer.writeObject(events);
+        } catch (Throwable var5) {
+            try {
+                serializer.close();
+            } catch (Throwable var4) {
+                var5.addSuppressed(var4);
+            }
+
+            throw var5;
+        }
+
+        serializer.close();
+    }
+
+    public static LocalEvent[] deserialize() throws IOException, ClassNotFoundException {
+        ObjectInputStream deserializer = new ObjectInputStream(new FileInputStream("data.bin"));
+
+        LocalEvent[] var6;
+        try {
+            Object[] event = (Object[])deserializer.readObject();
+            LocalEvent[] arr = new LocalEvent[event.length];
+            int i = 0;
+
+            while(true) {
+                if (i >= event.length) {
+                    var6 = arr;
+                    break;
+                }
+
+                arr[i] = (LocalEvent)event[i];
+                ++i;
+            }
+        } catch (Throwable var5) {
+            try {
+                deserializer.close();
+            } catch (Throwable var4) {
+                var5.addSuppressed(var4);
+            }
+
+            throw var5;
+        }
+
+        deserializer.close();
+        return var6;
+    }
+
+    private EventSerializer() {
+    }
+}
---
+++ src/main/java/com/example/gui_assignment/LocalEvent.java
@@ -0,0 +1,36 @@
+package com.example.gui_assignment;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+
+public class LocalEvent implements Serializable  {
+
+    private static final long serialVersionUID = 1L;
+
+    private LocalDate date;
+
+    private String description;
+
+    public LocalEvent(LocalDate date, String description) {
+        this.date = date;
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+
+    // this makes LocalEvents dispaly nicely in GUI
+    @Override
+    public String toString() {
+        return "At: " + date + description;
+    }
+
+}
